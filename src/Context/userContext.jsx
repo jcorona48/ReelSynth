9aa00bf135f58f6af9dcb329243fd940a7fd5f16
jcorona48 <105407871+jcorona48@users.noreply.github.com>
2023-10-25 20:40:16 -0400

    Return back

--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -7,6 +7,7 @@ export const UserContext = createContext()
 export default function UserProvider({children}) {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [token, setToken] = useState(null)
 
     return (
         <UserContext.Provider value={{user, setUser, loading, setLoading}}>
--- src/components/Login/SignIn/SignIn.jsx
+++ src/components/Login/SignIn/SignIn.jsx
@@ -4,11 +4,58 @@ import { useContext } from "react"
 import { useMutation, gql } from "@apollo/client"
 
 
+const query = gql`
+
+    mutation Login($input: LoginInput!) {
+        login(input: $input) {
+                token
+            }
+    }
+    
+`
 
 export default function SignIn(){
-    // const {setToken} = useContext(UserContext)
+    const {setToken} = useContext(UserContext)
+    const [Login, loading, error] = useMutation(query)
 
-    
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const form = new FormData(e.target)
+        const data = Object.fromEntries(form)
+        const {userName, password} = data
+
+        // username es un email?
+        const isEmail = /\S+@\S+\.\S+/.test(userName)
+
+        let token = null
+        if(isEmail){
+            console.log('es un email')
+            token = await Login({
+                variables: {
+                    input: {
+                        email: userName,
+                        password
+                    }
+                }
+            })
+        }else{
+            console.log('es un username')
+            token = await Login({
+                variables: {
+                    input: {
+                        userName,
+                        password
+                    }
+                }
+            })
+
+
+        }
+
+        if(token) setToken(token.data.login.token)
+        console.log(token?.data?.login?.token)
+        console.log(error)
+    }
     return(
         <div className="form-container sign-in">
             <form onSubmit={handleSubmit} aria-label="Iniciar Sesion">

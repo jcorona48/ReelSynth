449733497b19dd54f156fe70be2f2c62e0217e61
Jey_Slon <105397847+Dec-SOM@users.noreply.github.com>
2023-12-05 20:06:33 -0400

    change buttom content

--- src/components/Video/ButtomEpisode.jsx
+++ src/components/Video/ButtomEpisode.jsx
@@ -6,10 +6,10 @@ export default function ButtomEpisode() {
             <div className="container-buttomepisode">
                 <div className="buttom-episode">
                     <ul>
-                        <li><i className='fa-solid fa-backward'/><a href="#">Back</a></li> 
-                        <li><i className='fa-regular fa-send-back'/><a href="#">All Season</a></li>
-                        <li><i className='fa-regular fa-list'/><a href="#">All Episode</a></li>
-                        <li><i className='fa-solid fa-forward'/><a href="#">Next</a></li>
+                        <li className="back-none"><i className='fa-solid fa-backward'/><a href="#">Back</a></li> 
+                        <li className='season-none'><i className='fa-regular fa-send-back'/><a href="#">All Season</a></li>
+                        <li className='episode-none'><i className='fa-regular fa-list'/><a href="#">All Episode</a></li>
+                        <li className='next-none'><i className='fa-solid fa-forward'/><a href="#">Next</a></li>
                     </ul>
                 </div>
             </div>

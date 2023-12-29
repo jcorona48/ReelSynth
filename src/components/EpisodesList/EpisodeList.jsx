aab403473701c41fc2138de3708b4f10836e2bdc
Jey_Slon <105397847+Dec-SOM@users.noreply.github.com>
2023-12-29 11:55:33 -0400

    fix episode description

--- src/components/EpisodesList/EpisodeList.jsx
+++ src/components/EpisodesList/EpisodeList.jsx
@@ -29,7 +29,7 @@ export default function EpisodeList({season}) {
         }
     
     })
-    console.log(season)
+    
     return (
         <div className='SeasonList'>
             <>
--- src/components/Seasons/Season.jsx
+++ src/components/Seasons/Season.jsx
@@ -8,14 +8,16 @@ export default function Season({item, type}) {
             <main className='container-season-detail'>
                 <div className='season-detail'>
                     <h1>{item.title}</h1>
-                    <h2>{item.description}</h2>
                     <div className="season-info">
                         <h4>{type} {item.number}</h4>
                         { type === 'Season' ? <h4>Epidoses: {item.episodesCount}</h4> : null } 
                     </div>
+                    <div className='season-icon'>
+                        <i className="fa-regular fa-arrow-up-right-from-square" />
+                    </div>
                 </div>
-                <div className='season-icon'>
-                    <i className="fa-regular fa-arrow-up-right-from-square" />
+                <div className="season-description">
+                    <p>{item.description}</p>
                 </div>
             </main>
         </Link>
